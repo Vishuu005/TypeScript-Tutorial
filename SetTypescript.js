5e5acfe62afcdb03c9f13510dc324bb206142ccb
Vishuu005 <67571378+Vishuu005@users.noreply.github.com>
2022-08-18 08:54:03 +0530

    Commit
    New TypeScript Small Programme Generated

---
+++ SetTypescript.js
@@ -0,0 +1,18 @@
+// install node and npm
+//the install typescript using
+// npm install typescript -g
+// check by using tsc -v in terminal (but not in powershell)
+console.log("Hello in Typescript");
+// $ tsc Setuptypescript.ts
+// $ node Setuptypescript.js
+// Hello in Typescript
+// $ node Setuptypescript.ts
+// Hello in Typescript
+// dynamically-typed languages perform type checking at runtime,
+// while statically typed languages perform type checking at compile time.
+// JavaScript is a dynamically-typed language,
+var num1 = 104;
+console.log("num1", num1);
+var num2 = 78;
+console.log("num2", num2);
+var num4;
---
+++ ifelse.js
@@ -0,0 +1,19 @@
+// var num=23;
+// if(num>=18)
+// {
+//     console.log('u r ready to vote');
+// }
+// else
+// {
+//     console.log('u r not ready to vote');
+// }
+var num = 23;
+if (num >= 18) {
+    console.log('u r ready to vote');
+}
+else if (num = 10) {
+    console.log('congrates for first time vote');
+}
+else {
+    console.log('u r not ready to vote');
+}
