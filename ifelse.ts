ba9ae5c92bad306aa6ce9733825f581960976eb8
vishal <vishalpandey7575@gmail.com>
2022-08-18 08:44:12 +0530

    Add new file

---
+++ ifelse.ts
@@ -0,0 +1,154 @@
+// var num=23;
+
+// if(num>=18)
+// {
+//     console.log('u r ready to vote');
+// }
+// else
+// {
+//     console.log('u r not ready to vote');
+// }
+
+
+
+
+
+// var num=23;
+
+// if(num>=18)
+// {
+//     console.log('u r ready to vote');
+// }
+// else if(num=10)
+// {
+//     console.log('congrates for first time vote');
+// }
+// else
+// {
+//     console.log('u r not ready to vote');
+// }             
+
+
+
+// var num=17;
+// var num: number = 17;
+// var num2:string="17";
+
+// if(num>=18)
+// {
+//     console.log(`u r ready to vote `);
+// }
+
+// if(num>=18)
+// {
+//     console.log(`u r ready to vote `);
+// }
+// else
+// {
+//     console.log(`u r not ready to vote`);
+// }
+
+// if(num>18)
+// {
+//     console.log(`u r ready to vote `);
+// }
+// else if(num==18)
+// {
+//     console.log("congrats for 1st time vote");
+// }
+// else
+// {
+//     console.log(`u r not ready to vote`);
+// }
+
+
+// if(num>18)
+// {
+//     console.log(`u r ready to vote `);
+// }
+// else if(num==18)
+// {
+//     console.log("congrats for 1st time vote");
+// }
+// else
+// {
+//     console.log(`u r not ready to vote`);
+
+//     if (num==17) {
+//         console.log("wait for 1 year to vote");
+//     }
+// }
+
+
+// var marks:number=97;
+// if (marks <= 100 && marks>=90)
+// {
+//     console.log("A grade");  
+// }
+
+// var choice = 2;
+// switch (choice) {
+//     case 1:
+//         console.log("u choosed 1");
+//         break;
+//     case 2:
+//         console.log("u choosed 2");
+//         break;
+//     case 3:
+//         console.log("u choosed 3");
+//         break;
+//     default: console.log("invalid choice");
+//         break;
+// }
+
+// for (let i = 1; i <=10; i++) 
+// {
+//     console.log(" i=",i);
+// }
+// var a = 1
+// while (a<=10) 
+// {
+//     console.log(`a=`, a);
+//     a++;
+// }
+// do {
+//     console.log(`a=`, a);
+//     a++;
+// } while (a<=10);
+
+
+// var fruits: string[] = ["mango", "apple", "strawberry", "banana"];
+
+// for (const f of fruits) 
+// {
+//     console.log(f);
+// }
+
+// for (const i in fruits) 
+// {
+//     console.log(`at index `,i," fruit is ",fruits[i]);
+// }
+
+// var veg=new Array();
+// veg.push("potato");
+
+// console.log(`veg=`, veg);
+
+//2D array in typescript
+
+//pre-defined
+
+// var matr1: number[][] = [[10, 20, 30], [40, 50, 60]];
+
+// console.log(matr1[1][2])
+
+// for(let i=0;i<matr1.length;i++)//2 row
+// {
+//     for(let j=0;j<matr1[0].length;j++)//3 column
+//     {
+//         console.log(matr1[i][j]);
+//     }
+//     console.log(`\n`);
+// }
+
+// console.table(matr1);
